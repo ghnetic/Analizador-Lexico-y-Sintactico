03a07a36adcf3733f75871e7220fb0d8b14ed1e9
yohelis29 <yohelisbonit@gmail.com>
2022-08-11 20:11:02 -0600

    Funcion Concatenar y Repeticion

--- regla_semantica.py
+++ regla_semantica.py
@@ -7,8 +7,22 @@ class ReglaSemantica(Transformer):
     def __init__(self):
         self.var = {}
     
+    #Funcion para la operacion Sumar
     def sumar(self, valor1, valor2):
         return float(valor1) + float(valor2)
 
+    #Funcion para la operacion Restar
     def restar(self, valor1, valor2):
         return float(valor1) - float(valor2)
+        
+    #Funcion para la operacion Concatenar
+    def concatenar(self,valor1, valor2):
+        return (self.cleanParam(self.var[valor1]) + " " + self.cleanParam(self.var[valor2]))
+
+    def concatenacion(self, valor1, valor2):
+        return self.cleanParam(self.var[valor1]) + " " + valor2
+    
+    #Funcion para la operacion repeticion 
+    def repeticion(self, num, palabra):
+        return int(num) * self.cleanParam(palabra)
+ 
