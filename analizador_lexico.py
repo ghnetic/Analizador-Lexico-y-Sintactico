d51cc5b6cabacd6cbd8c8f5e8cb2141f8648123f
Antony Brenes <antonybrenescruz@gmail.com>
2022-08-12 23:09:18 -0600

    Terminando analizador lexico

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -1,23 +1,126 @@
-#Importacion de expresiones regulares
+# Importacion de expresiones regulares
 import re
+
 
 class AnalizadorLexico:
     def __init__(self, reader, name):
-        self.reader=reader
-        self.archivo=name
-        self.texto=self.reader.texto
-        self.tokens={}
-    
+        self.reader = reader
+        self.archivo = name
+        self.patterns = []
+        self.texto = self.reader.texto
+        self.tokens = {}
+        self.t = ""
+        self.contador = 0
+
     def contadorLineas(self):
-        linea=0
-        texto=re.split(r"\n", self.texto)
+        linea = 0
+        texto = re.split(r"\n", self.texto)
         for numLinea in texto:
             print("Probando contador...")
-            print(re.split(r"\s", numLinea.replace("\'",'')))
-            linea+=1
-            self.tokens[linea]=re.split(r"\s", numLinea)
+            print(re.split(r"\s", numLinea.replace("\'", '')))
+            linea += 1
+            self.tokens[linea] = re.split(r"\s", numLinea)
         print(self.tokens)
         print(linea)
         return self
 
-    
+    def tablaLexico(self):
+        patterns = []
+
+        for key in self.tokens:
+            for token in self.tokens[key]:
+                if(token != ''):
+
+                    if self.contador > 0:
+                        if re.match(r"^[a-zA-Z][a-zA-Z0-9_]*$", token):
+                            if re.match(r"^[a-zA-Z][a-zA-Z0-9_]*\'$", token):
+
+                                self.t = self.t + " " + \
+                                    token.replace("'", '')
+                                self.contador = 0
+
+                                patterns.append(
+                                    [key, self.t, "Cadena Identificada"])
+                                self.t = ""
+                            self.t = self.t + " " + token
+                            self.contador = 0
+
+                    elif re.match(r"^[a-zA-Z][a-zA-Z0-9_]*$", token):
+                        patterns.append(
+                            [key, token, "Identificador"])
+
+                    elif re.match(r"^\'[^\']*\'$", token):
+                        patterns.append(
+                            [key, token, "Cadena Identificada"])
+
+                    elif re.match(r"^=$", token):
+                        patterns.append(
+                            [key, token, "Operador de asignacion"])
+
+                    elif re.match(r"^\$$", token):
+                        patterns.append(
+                            [key, token, "Fin de linea identificado"])
+
+                    elif re.match(r"^\d+(\.\d+)?$", token):
+                        patterns.append(
+                            [key, token, "Numero identificado"])
+
+                    elif re.match(r"^\+*$", token):
+                        patterns.append([key, token, "Operador de Suma"])
+
+                    elif re.match(r"^\-*$", token):
+                        patterns.append([key, token, "Operador de Resta"])
+
+                    elif re.match(r"^\**$", token):
+                        patterns.append([key, token, "Operador de Repeticion"])
+
+                    elif re.match(r"^\.$", token):
+                        patterns.append(
+                            [key, token, "Operador de concatenacion"])
+
+                    elif re.match(r"^imprimirResultado\([a-z0-9+*\-'.]*\)$", token):
+                        patterns.append(
+                            [key, token, "Funcion de impresion"])
+
+                    elif re.match(r"^\'[a-zA-Z][a-zA-Z0-9_]*$", token):
+                        print("entro1")
+                        self.t = self.t + token.replace("'", '')
+                        self.contador = 1
+
+                    elif re.match(r"^[a-zA-Z][a-zA-Z0-9_]*\'$", token):
+                        print("entro3")
+                        self.t = self.t + " " + token.replace("'", '')
+                        print(self.t)
+                        self.contador = 0
+
+                        patterns.append(
+                            [key, self.t, "Cadena Identificada"])
+                        self.t = ""
+
+                    else:
+                        quit(
+                            "Error: \n\ttoken desconocido en la linea %s: %s \n\n" % (
+                                self.buscarLineaErrorToken(token),
+                                token
+                            )
+                        )
+
+        self.patterns = patterns
+        return self
+
+    def buscarLineaErrorToken(self, token):
+        lineaError = 0
+
+        f = open(self.fileName, 'r')
+        for linea in f:
+            lineaError += 1
+            if re.match(r'^.*%s.*$' % self.prevenir(token), linea):
+
+                break
+        f.close()
+        return lineaError
+
+    def prevenir(self, token):
+
+        if re.match(r'[\+\*\.\(\)\{\}\[\]\\\<\>]', token):
+            return '\\%s' % token
--- main.py
+++ main.py
@@ -2,36 +2,39 @@ from analizador_lexico import *
 from analizador_sintactico import tabulate
 import sys
 
+
 class Reader():
-    def __init__(self): 
+    def __init__(self):
         self.instruction = None
         self.fileName = None
         self.text = None
 
     def read(self):
-        #Para leer el archivo .txt
+        # Para leer el archivo .txt
         self.params = sys.argv[1:]
 
         try:
             if len(self.params) == 1:
-                self.fileName =  self.params[0]
-                f = open(self.fileName,'r')
+                self.fileName = self.params[0]
+                f = open(self.fileName, 'r')
                 self.text = f.read()
                 f.close()
 
             else:
                 quit("Error: cantidad de parametros no son validos")
         except FileNotFoundError as e:
-            quit("Error %s"%e)
-        return self 
+            quit("Error %s" % e)
+        return self
+
 
 reader = Reader().read()
 name = Reader().read().fileName
 
 if reader.instruction == None:
     print("\n* * * * * * * * * * A N A L I Z A D O R   L E X I C O * * * * * * * * *\n")
-    tokens = AnalizadorLexico(reader, name ).convertirArchivo().tablaAnalizadorLexico().patrones
-    encabezadoTabla = ['# Linea','Token','Descripcion']
+    tokens = AnalizadorLexico(
+        reader, name).convertirArchivo().tablaLexico().patrones
+    encabezadoTabla = ['# Linea', 'Token', 'Descripcion']
     if len(tokens) > 0:
         print(tabulate(tokens, encabezadoTabla, tablefmt="fancy_grid"))
     else:
@@ -40,7 +43,3 @@ if reader.instruction == None:
     AnalizadorSintactico(reader.text).run()
 else:
     quit("Error")
-
-
-
-
